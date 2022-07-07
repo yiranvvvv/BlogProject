79e9192dccb72848e2771733cfe7fa91d929a920
yiran <yiran.wei@siemens.com>
2022-07-07 15:40:22 +0800

    c6

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
+#from app import routes, models, errors
+
+
 csrf = CSRFProtect()
 
 app = Flask(__name__)
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
+from crypt import methods
 from flask_login import current_user, login_required, login_user, logout_user
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
-from app.forms import LoginForm, ResitrationForm
+from app.forms import EditProfileForm, LoginForm, ResitrationForm
 from app.models import User, Post
-
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -61,8 +62,49 @@ def register():
         flash('Sorry, registion unsuccessful.')
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test Post 1'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 # TODO: 
 ''' requires admin rights '''
 @app.route('/display')
 def display():
     return render_template('display.html', users = User.query.all())
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
